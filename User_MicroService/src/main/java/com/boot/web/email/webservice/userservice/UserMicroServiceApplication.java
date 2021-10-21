030f353cc4f6ad452595ece0b6c8b5cef01973eb
Milan_Balar <Milan_Balar@192.168.1.29>
2021-10-21 07:56:32 +0530

    Step :1 It's 1st microservice

---
+++ User_MicroService/src/main/java/com/boot/web/email/webservice/userservice/UserMicroServiceApplication.java
@@ -0,0 +1,22 @@
+package com.boot.web.email.webservice.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class UserMicroServiceApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMicroServiceApplication.class, args);
+        System.out.println("####-----boot is ready----####");
+    }
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+}
---
+++ User_MicroService/src/test/java/com/boot/web/email/webservice/userservice/UserMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.web.email.webservice.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
